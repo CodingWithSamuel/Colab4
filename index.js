e48b34eaec3b51d7778e78eb41e968231facbfaa
CodingWithSamuel <samu1800.so@gmail.com>
2024-09-03 09:33:12 -0300

    css/Js Feitos Precisa de ajustes

---
+++ index.js
@@ -0,0 +1,19 @@
+const profiles = document.querySelectorAll('.profile img');
+
+        profiles.forEach(profile => {
+            profile.addEventListener('mouseover', () => {
+                profile.style.transform = 'scale(1.2) rotate(360deg)';
+            });
+
+            profile.addEventListener('mouseout', () => {
+                profile.style.transform = 'scale(1)';
+            });
+
+            profile.addEventListener('click', () => {
+                profile.style.transform = 'scale(1.2) rotate(360deg)';
+            });
+        });
+
+
+
+/* JS SCRIPT FAZENDO A ANIMAÇÃO DE ROTAÇÂO EM CADA IMAGEM*/
