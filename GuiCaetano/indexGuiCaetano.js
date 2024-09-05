f2b69e1a6e0b99009ce81ecc1c3dc1d0c317c9ab
EUsouguido <guicaeatanobassan@gmail.com>
2024-09-05 01:48:50 -0300

    mudei minha pagina

--- GuiCaetano/indexGuiCaetano.js
+++ GuiCaetano/indexGuiCaetano.js
@@ -8,3 +8,12 @@ function updateProgressBar(progressBarId, progressPercentId, percent) {
 updateProgressBar('progressBar1', 'progressPercent1', 25); // BARRA 1
 updateProgressBar('progressBar2', 'progressPercent2', 100); // BARRA 2
 updateProgressBar('progressBar3', 'progressPercent3', 25); //BARRA 3
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggleButton');
+    const imgTextContainer = document.querySelector('.img-text-container');
+
+    toggleButton.addEventListener('click', () => {
+        imgTextContainer.classList.toggle('clicked');
+    });
+});
